6a6fb18c4e14afb3bc273dd55c75c7c7479d930d
dauntless <suntopmultimedia@gmail.com>
2022-07-19 13:30:37 +0100

    debug

--- timetable/settings/production.py
+++ timetable/settings/production.py
@@ -5,7 +5,7 @@ import os, dj_database_url
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', None)
+DEBUG = True
 
 MIDDLEWARE.insert(2, "whitenoise.middleware.WhiteNoiseMiddleware")
 # Database
